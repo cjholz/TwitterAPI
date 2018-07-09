5d66a0e72ed71c2d4c5439269d144d811dd11472
cjholz <cjholz@mit.edu>
2018-07-09 04:02:55 -0400

    simple run

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from twitter_app.app import app
 
-app.run(
-    host="0.0.0.0",
-    port=8888,
-    use_reloader=True,
-    use_debugger=True,
-    threaded=False
-)
+
+app.run()
+#app.run(
+    #host="0.0.0.0",
+    #port=5000,
+    #use_reloader=True,
+    #use_debugger=True,
+    #threaded=False
+#)
